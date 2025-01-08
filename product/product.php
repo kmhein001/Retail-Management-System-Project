03be69ea53d7dd28051e2e5be34c7a0b39e1350e
Kaung Myat Hein <km.hein001@gmail.com>
2025-01-08 10:37:44 +0630

    Add product.php despite .gitignore rule

---
+++ product/product.php
@@ -0,0 +1,128 @@
+<?php
+$L_general = "product";
+$L_img = $L_general."/";
+
+$C_general = "Product";
+$F_SQL = $C_general."s";
+
+$H_Title_1 = $C_general . " List";
+$H_Title_Link_1 = $L_general . "_list.php";
+
+$H_Title_2 = "Add " . $C_general;
+$H_Title_Link_2 = $L_general . "_add.php";
+
+$H_Title_3 = "Edit " . $C_general;
+$H_Title_Link_3 = $L_general . "_edit.php";
+
+$H_Title_Link_4 = $L_general . "_delete.php";
+$H_Title_5 = $C_general. "Detail";
+$H_Title_Link_5 = $L_general . "_detail.php";
+$H_Title_Link_6 = $L_general . "_update.php";
+$H_Title_Link_7 = $L_general . "_add_process.php";
+
+$B_Title_1 = "Back to List";
+$H_Title_D_1 = "Manage your " . $C_general;
+
+$SQL_Title_1 = "ID";
+$SQL_Title_1_1= $C_general ." " .$SQL_Title_1;
+$SQL_1 = $C_general . "$SQL_Title_1";
+
+$SQL_Title_2 = $C_general . " Name";
+$SQL_2 = $C_general . "Name";
+
+$SQL_Title_3 = "Image";
+$SQL_3 = "ImageFile";
+$H_Title_6 = $C_general . $SQL_Title_3;
+
+$SQL_Title_4 = "SKU";
+$SQL_4 = "SKU";
+
+$SQL_Title_4_1 = "Bar Code";
+
+$SQL_Title_5 = "Brand Name";
+$SQL_5 = "BrandName";
+
+$SQL_Title_6 = "Category Name";
+$SQL_6 = "CategoryName";
+
+$SQL_Title_7 = "Subcategory Name";
+$SQL_7 = "SubcategoryName";
+
+$SQL_Title_8 = "Price";
+$SQL_8 = "Price";
+
+$SQL_Title_9 = "Quantity";
+$SQL_9 = "Quantity";
+
+$SQL_Title_10 = "Unit";
+$SQL_10 = "Unit";
+
+$SQL_Title_11 = "Created By";
+$SQL_11 = "CreatedBy";
+
+$SQL_Title_12 = "Status";
+$SQL_12 = "Status";
+
+$Title_13 = "Actions";
+
+$V_C_icons = "../vendors/@coreui/icons/";
+$V_C_I_svg = $V_C_icons . "svg/";
+$V_C_I_S_free = $V_C_I_svg . "free.svg";
+
+$L_Title_other_1 = "brand";
+$L_Title_other_1_1 =$L_Title_other_1."s";
+
+$Title_other_1 = "Brand";
+$Title_with_1_1 = "Select ".$Title_other_1;
+$Title_with_1_2 = $Title_other_1." Name";
+
+$SQL_T_Table_1 = $Title_other_1."s";
+$SQL_T_Table_1_1 = $Title_other_1."ID";
+$SQL_T_Table_1_2 = $Title_other_1."Name";
+
+$L_Title_other_2 = "category";
+$L_Title_other_2_1 = "categories";
+
+$Title_other_2 = "Category";
+$Title_with_2_1 = "Select ".$Title_other_2;
+$Title_with_2_2 = $Title_other_2." Name";
+
+$SQL_T_Table_2 = $Title_other_2;
+$SQL_T_Table_2_1 = $Title_other_2."ID";
+$SQL_T_Table_2_2 = $Title_other_2."Name";
+
+$L_Title_other_3 = "subcategory";
+$L_Title_other_3_1 = "subcategories";
+
+$Title_other_3 = "Subcategory";
+$Title_with_3_1 = "Select ".$Title_other_3;
+$Title_with_3_2 = $Title_other_3." Name";
+
+$SQL_T_Table_3 = $Title_other_3;
+$SQL_T_Table_3_1 = $Title_other_3."ID";
+$SQL_T_Table_3_2 = $Title_other_3."Name";
+
+$L_Title_other_4 = "unit";
+$L_Title_other_4_1 =$L_Title_other_4."s";
+
+$Title_other_4 = "Unit";
+$Title_with_4_1 = "Select ".$Title_other_4;
+$Title_with_4_2 = $Title_other_4." Name";
+
+$SQL_T_Table_4 = "Measurement".$Title_other_4."s";
+$SQL_T_Table_4_1 = $Title_other_4."ID";
+$SQL_T_Table_4_2 = "ShortWord";
+
+
+$N_id = "id";
+$N_name = "name";
+$N_sku = "SKU";
+$N_bname = "bname";
+$N_cname = "cname";
+$N_sname = "sname";
+$N_price = "price";
+$N_qty = "qty";
+$N_unit = "unit";
+$N_status = "status";
+$N_cby = "cby";
+?>
